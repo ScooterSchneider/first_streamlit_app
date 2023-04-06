9cbf596c8ae51706cfb728293a656a64e17a0eea
Scooter Schneider <scooter.schneider@icloud.com>
2023-04-06 09:47:39 -0400

    Update streamlit_app.py
    Added Icons

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,7 +2,7 @@
 import streamlit;
 streamlit.title('My Parent new Healthy Diner');
 streamlit.header('Breakfast Menu');
-streamlit.text('Healthy Oatmeal');
-streamlit.text('Chewey Kale - So Gross');
-streamlit.text('Hard Boiled Chicken Poop, Eggs');
-
+streamlit.text('🥣 Healthy Oatmeal');
+streamlit.text('🥗 Chewey Kale - So Gross');
+streamlit.text('🐔 Hard Boiled Chicken Poop, Eggs');
+streamlit.text('🥑🍞 Avacado Toast');
