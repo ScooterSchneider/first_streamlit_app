62c5a57052802b2a0ce44f5fedea27e6929437bb
Scooter Schneider <scooter.schneider@icloud.com>
2023-04-06 09:44:21 -0400

    Update streamlit_app.py
    Funny Descriptors

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,4 +1,8 @@
 
 import streamlit;
 streamlit.title('My Parent new Healthy Diner');
-streamlit.title('May the fetecinni be with you');
+streamlit.header('Breakfast Menu');
+streamlit.text('Healthy Oatmeal');
+streamlit.text('Chewey Kale - So Gross');
+streamlit.text('Hard Boiled Chicken Poop, Eggs');
+
