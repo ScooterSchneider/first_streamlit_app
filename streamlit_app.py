2125f8b58ff1a72c94519d36d21ef300032ec85f
Scooter Schneider <scooter.schneider@icloud.com>
2023-04-07 15:05:35 -0400

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -29,7 +29,6 @@ my_data_rows = my_cur.fetchall()
 
 # my_fruit_list = pandas.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt");
 # my_fruit_list = my_fruit_list.set_index('Fruit')
-my_data_rows = my_data_rows.set_index('FRUIT_NAME');
 
 # Let's put a pick list here so they can pick the fruit they want to include 
 # streamlit.multiselect("Pick some fruits:", list(my_fruit_list.index))
