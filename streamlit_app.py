6d54bab8222ad0bfae8a5f57b41ef6594a01329d
Scooter Schneider <scooter.schneider@icloud.com>
2023-04-07 15:35:16 -0400

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -18,29 +18,16 @@ streamlit.header('🍌🥭 Build Your Own Fruit Smoothie 🥝🍇')
 
 
 # Import some fruit
-
-my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
-my_cur = my_cnx.cursor ()
-my_cur.execute("select * from fruit_load_list")
-# my_data_row = my_cur.fetchone()
-my_data_rows = my_cur.fetchall()
-# streamlit.header("The fruit load list contains:")
-# streamlit.dataframe(my_data_rows)
-
 my_fruit_list = pandas.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt");
 my_fruit_list = my_fruit_list.set_index('Fruit')
 
 # Let's put a pick list here so they can pick the fruit they want to include 
-# streamlit.multiselect("Pick some fruits:", list(my_fruit_list.index))
-# fruits_selected = streamlit.multiselect("Pick some fruits:", list(my_data_rows))
 fruits_selected = streamlit.multiselect("Pick some fruits:", list(my_fruit_list.index),['Avocado','Strawberries'])
-# streamlit.stop();
 
 # Filter on selected
 fruits_to_show = my_fruit_list.loc[fruits_selected]
 
 # Display the table on the page.
-# streamlit.dataframe(my_fruit_list);
 streamlit.dataframe(fruits_to_show);
 
 #New Section to display fruityvice api response
@@ -53,6 +40,15 @@ fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + fruit_c
 # take the json version of the response and normalize it
 fruityvice_normalized = pandas.json_normalize(fruityvice_response.json ()) #output it the screen as a table
 streamlit.dataframe (fruityvice_normalized)
+streamlit.stop();
+
+my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
+my_cur = my_cnx.cursor ()
+my_cur.execute("select * from fruit_load_list")
+# my_data_row = my_cur.fetchone()
+my_data_rows = my_cur.fetchall()
+# streamlit.header("The fruit load list contains:")
+# streamlit.dataframe(my_data_rows)
 
 add_my_fruit = streamlit.text_input('What would you like to add?')
 streamlit.write('Thanks for adding ', add_my_fruit)
