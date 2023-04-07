e1c1d11a02ab7543f5930200b96bc4baff92ccb6
Scooter Schneider <scooter.schneider@icloud.com>
2023-04-07 14:59:13 -0400

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -23,19 +23,19 @@ my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
 my_cur = my_cnx.cursor ()
 my_cur.execute("select * from fruit_load_list")
 # my_data_row = my_cur.fetchone()
-my_data_rows = my_cur.fetchall().to_pandas()
+my_data_rows = my_cur.fetchall()
 # streamlit.header("The fruit load list contains:")
 # streamlit.dataframe(my_data_rows)
 
 my_fruit_list = pandas.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt");
-my_fruit_list=my_data_rows;
+my_fruit_list=my_data_rows.to_pandas();
 my_fruit_list = my_fruit_list.set_index('Fruit')
 
 
 # Let's put a pick list here so they can pick the fruit they want to include 
 # streamlit.multiselect("Pick some fruits:", list(my_fruit_list.index))
 fruits_selected = streamlit.multiselect("Pick some fruits:", list(my_fruit_list.index),['Avocado','Strawberries'])
-# streamlit.stop();
+streamlit.stop();
 
 # Filter on selected
 fruits_to_show = my_fruit_list.loc[fruits_selected]
