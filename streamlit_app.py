8c3483b49bf91a09b0ebe5c1aca38773d860d6e4
Scooter Schneider <scooter.schneider@icloud.com>
2023-04-07 14:58:31 -0400

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -18,8 +18,19 @@ streamlit.header('🍌🥭 Build Your Own Fruit Smoothie 🥝🍇')
 
 
 # Import some fruit
+
+my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
+my_cur = my_cnx.cursor ()
+my_cur.execute("select * from fruit_load_list")
+# my_data_row = my_cur.fetchone()
+my_data_rows = my_cur.fetchall().to_pandas()
+# streamlit.header("The fruit load list contains:")
+# streamlit.dataframe(my_data_rows)
+
 my_fruit_list = pandas.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt");
+my_fruit_list=my_data_rows;
 my_fruit_list = my_fruit_list.set_index('Fruit')
+
 
 # Let's put a pick list here so they can pick the fruit they want to include 
 # streamlit.multiselect("Pick some fruits:", list(my_fruit_list.index))
@@ -44,16 +55,8 @@ fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + fruit_c
 fruityvice_normalized = pandas.json_normalize(fruityvice_response.json ()) #output it the screen as a table
 streamlit.dataframe (fruityvice_normalized)
 
-my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
-my_cur = my_cnx.cursor ()
-my_cur.execute("select * from fruit_load_list")
-# my_data_row = my_cur.fetchone()
-my_data_rows = my_cur.fetchall()
-streamlit.header("The fruit load list contains:")
-streamlit.dataframe(my_data_rows)
-
 add_my_fruit = streamlit.text_input('What would you like to add?')
 streamlit.write('Thanks for adding ', add_my_fruit)
 
 #This will not work correctly, but just go with it for now
-my_cur.execute("insert into fruit_load_list values('from streamlit')")
+my_cur.execute("insert into fruit_load_list values(add_my_fruit)")
