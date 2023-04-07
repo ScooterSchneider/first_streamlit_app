7080f8c111e3f3fcfb57650cd67a0d18b9f60f76
Scooter Schneider <scooter.schneider@icloud.com>
2023-04-07 12:17:00 -0400

    Update streamlit_app.py
    sdfsadfds

--- streamlit_app.py
+++ streamlit_app.py
@@ -29,11 +29,16 @@ streamlit.dataframe(fruits_to_show);
 
 #New Section to display fruityvice api response
 streamlit.header("Fruityvice Fruit Advice!")
-fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
-# streamlit.text(fruityvice_response)
-# streamlit.text(fruityvice_response.json())
+# fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
+# # streamlit.text(fruityvice_response)
+# # streamlit.text(fruityvice_response.json())
 
-# Format to be readable
-fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
-# Print the new layout
-streamlit.dataframe(fruityvice_normalized)
+# # Format to be readable table
+# fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
+# # Print the new layout
+# streamlit.dataframe(fruityvice_normalized)
+
+fruityvice response = requests.get("https://fruityvice.com/api/fruit/" + "kiwi")
+# take the json version of the response and normalize it
+fruityvice_normalized = pandas.json_normalize(fruityvice_response.json ()) #output it the screen as a table
+streamlit.dataframe (fruityvice_normalized)
