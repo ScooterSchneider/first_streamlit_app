41c2d33f1856663afcbd191318d5c5ad4752fcd7
ScooterAllydvm <103957183+ScooterAllydvm@users.noreply.github.com>
2023-04-08 13:53:09 -0400

    change

--- streamlit_app.py
+++ streamlit_app.py
@@ -20,7 +20,12 @@ streamlit.header('🍌🥭 Build Your Own Fruit Smoothie 🥝🍇')
 # Import some fruit
 my_fruit_list = pandas.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt");
 my_fruit_list = my_fruit_list.set_index('Fruit')
+streamlit.dataframe (my_fruit_list)
 
+if streamlit.button ('Get Fruit Load List'):
+    my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
+    my_data_rows = get_fruit_load_list()
+    streamlit.dataframe (my_data_rows)
 # Let's put a pick list here so they can pick the fruit they want to include 
 fruits_selected = streamlit.multiselect("Pick some fruits:", list(my_fruit_list.index),['Avocado','Strawberries'])
 
@@ -51,6 +56,7 @@ except URLError as e:
     streamlit.error();
     
 streamlit. header ("The fruit load list contains:") #Snowflake-related functions
+
 def get_fruit_load_list():
     with my_cnx.cursor() as my_cur:
         my_cur.execute("select * from fruit_load_list")
@@ -74,3 +80,4 @@ if streamlit.button('Add a Fruit to the List'):
     my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
     back_from_function = insert_row_snowflake(add_my_fruit)
     streamlit.text(back_from_function)
+
