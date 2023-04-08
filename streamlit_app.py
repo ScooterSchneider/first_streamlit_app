8760d3708aef9b99792126e784020442c728f0b3
ScooterAllydvm <103957183+ScooterAllydvm@users.noreply.github.com>
2023-04-08 13:58:03 -0400

    modified:   streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -7,30 +7,20 @@ from urllib.error import URLError;
 
 # HEADER AREA
 streamlit.title('My Parent new Healthy Diner');
-# streamlit.header('Breakfast Menu');ls
-# streamlit.text('🥣 Healthy Oatmeal');
-# streamlit.text('🥗 Chewey Kale - So Gross');
-# streamlit.text('🐔 Hard Boiled Chicken Poop, Eggs');
-# streamlit.text('🥑🍞 Avacado Toast');
+streamlit.header('Breakfast Menu');
+streamlit.text('🥣 Healthy Oatmeal');
+streamlit.text('🥗 Chewey Kale - So Gross');
+streamlit.text('🐔 Hard Boiled Chicken Poop, Eggs');
+streamlit.text('🥑🍞 Avacado Toast');
 
 #  BUILD YOUR OWN
 streamlit.header('🍌🥭 Build Your Own Fruit Smoothie 🥝🍇')
 
 
-def get_fruit_load_list():
-    with my_cnx.cursor() as my_cur:
-        my_cur.execute("select * from fruit_load_list")
-        return my_cur.fetchall()
-
 # Import some fruit
 my_fruit_list = pandas.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt");
 my_fruit_list = my_fruit_list.set_index('Fruit')
-streamlit.dataframe (my_fruit_list)
 
-if streamlit.button ('Get Fruit Load List'):
-    my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
-    my_data_rows = get_fruit_load_list()
-    streamlit.dataframe (my_data_rows)
 # Let's put a pick list here so they can pick the fruit they want to include 
 fruits_selected = streamlit.multiselect("Pick some fruits:", list(my_fruit_list.index),['Avocado','Strawberries'])
 
@@ -61,7 +51,6 @@ except URLError as e:
     streamlit.error();
     
 streamlit. header ("The fruit load list contains:") #Snowflake-related functions
-
 def get_fruit_load_list():
     with my_cnx.cursor() as my_cur:
         my_cur.execute("select * from fruit_load_list")
@@ -85,4 +74,3 @@ if streamlit.button('Add a Fruit to the List'):
     my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
     back_from_function = insert_row_snowflake(add_my_fruit)
     streamlit.text(back_from_function)
-
